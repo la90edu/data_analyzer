3a7f28170fa14bf35faea4cb79b163c8bb5c338a
Lael Driquez <laeldriquez@gmail.com>
2025-03-18 14:15:30 +0200

    little fix

--- pages/Tipoah_Comparison.py
+++ pages/Tipoah_Comparison.py
@@ -4,59 +4,66 @@ import init
 import compare_multipul_groups
 from class_school_info import SchoolInfo
 
-df=init.init()
+                                 
 
-st.title('ניתוח מדדי טיפוח  ')
 
-unique_lamas = df["tipoh"].unique().tolist()
-chosen = st.selectbox('Select tipoh sector', unique_lamas)
+df = init.init()
 
-schoolInfo = SchoolInfo(df[df['tipoh']==chosen])
-fig_ici=schoolInfo.get_fig_ici("ici")
-fig_risc=schoolInfo.get_fig_risc("risc")
+st.title('ניתוח מדדי טיפוח ')
+
+# יוצר חלוקה לדאטא פריימס שונים ע"פ למס שונים
+df_tipoh_dict = {tipoh: df[df['tipoh'] == tipoh] for tipoh in df['tipoh'].unique()}
+
+compare_multipul_groups.show_comare_multipul_groups_tipoh(
+    "מיקוד שליטה פנימית", "ici",
+    df_tipoh_dict[4], "טיפוח 4",
+    df_tipoh_dict[5], "טיפוח 5",
+    df_tipoh_dict[6], "טיפוח 6",
+    df_tipoh_dict[7], "טיפוח 7",
+    df_tipoh_dict[8], "טיפוח 8",
+    df_tipoh_dict[9], "טיפוח 9",
+    df_tipoh_dict[10], "טיפוח 10"
+)
+
+compare_multipul_groups.show_comare_multipul_groups_tipoh(
+    "חוסן", "risc",
+    df_tipoh_dict[4], "טיפוח 4",
+    df_tipoh_dict[5], "טיפוח 5",
+    df_tipoh_dict[6], "טיפוח 6",
+    df_tipoh_dict[7], "טיפוח 7",
+    df_tipoh_dict[8], "טיפוח 8",
+    df_tipoh_dict[9], "טיפוח 9",
+    df_tipoh_dict[10], "טיפוח 10"
+)
+#     tipoh: df[df['tipoh'] == tipoh] 
+#     for tipoh in sorted(df['tipoh'].unique(), key=lambda x: float(x))
+# }
+
+
+
+
+
+unique = sorted(df["tipoh"].unique().tolist())
+
+st.markdown(
+        """
+        <style>
+        div[data-testid="stSelectbox"] {
+                text-align: right;
+                direction: rtl;
+                width: 150px; /* Reduced width from 200px to 150px */
+                margin-left: auto;
+                margin-right: 0;
+        }
+        div[data-testid="stSelectbox"] > label > p {
+                text-align: right;
+                direction: rtl;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+)
+tipoh_that_was_chosen = st.selectbox('בחרו מדד טיפוח', unique)
+schoolInfo = SchoolInfo(df[df['tipoh']==tipoh_that_was_chosen])
 fig_spider=schoolInfo.get_fig_spider()
-
-
-
-# יצירת שלוש עמודות
-col1, col2 = st.columns(2)
-# הצגת כל גרף בתוך עמודה משלו
-with col1:
-        st.plotly_chart(fig_ici, key="unique_key_ici", use_container_width=True)
-with col2:
-        st.plotly_chart(fig_risc, key="unique_key_risc", use_container_width=True)
-
-st.plotly_chart(fig_spider, key="unique_key_spider", use_container_width=True)
-    
-
-
-# import streamlit as st
-# import pandas as pd
-# import init
-# import compare_multipul_groups
-# from class_school_info import SchoolInfo
-
-# df=init.init()
-
-# st.title("ניתוח על פי מדד טיפוח" )
-
-# unique_tipoh = df["tipoh"].unique().tolist()
-# tipoh_that_was_chosen = st.selectbox('Select tipoh sector', unique_tipoh)
-# st.dataframe(df[df['tipoh']==tipoh_that_was_chosen])
-# schoolInfo = SchoolInfo(df[df['lamas']==tipoh_that_was_chosen])
-# fig_ici=schoolInfo.get_fig_ici("ici")
-# fig_risc=schoolInfo.get_fig_risc("risc")
-# fig_spider=schoolInfo.get_fig_spider()
-
-
-
-
-# # יצירת שלוש עמודות
-# col1, col2 = st.columns(2)
-# # הצגת כל גרף בתוך עמודה משלו
-# with col1:
-#         st.plotly_chart(fig_ici, key="unique_key_ici", use_container_width=True)
-# with col2:
-#         st.plotly_chart(fig_risc, key="unique_key_risc", use_container_width=True)
-
-# st.plotly_chart(fig_spider, key="unique_key_spider", use_container_width=True)
+st.plotly_chart(fig_spider, key="tipoh_key_spider", use_container_width=True)
